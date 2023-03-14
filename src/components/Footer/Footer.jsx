cfc71cbff3fdf73b830be9b0a35366406f2bebdb
Rick Allen Magbanua <ramnashor2@gmail.com>
2023-03-14 11:53:32 +0800

    Changes to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import React from 'react';
 
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import '../../styles/footer.css';
+
+const quickLinks = [
+  {
+    path:'/about',
+    display:'About'
+  },
+  {
+    path:'#',
+    display:'Privacy Policy'
+  },
+  {
+    path:'/cars',
+    display:'Car Listing'
+  },
+  {
+    path:'/blogs',
+    display:'Blogs'
+  },
+  {
+    path:'/contact',
+    display:'Contact'
+  },
+]
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <Container>
+      <Row>
+        <Col lg='4' md='4' sm='12'>
+        <div className="logo footer__logo">
+                <h1>
+                  <Link to='/home' className='d-flex align-items-center gap-2'>
+                    <i class="ri-car-line"></i>
+                    <span>
+                      CarXpress <br/>
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+              <p className="footer__logo-content">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, ab!
+              </p>
+        </Col>
+        <Col lg='2' md='4' sm='6'>
+          <div className="mb-4">
+            <h5 className="footer__link-title">Quick Links</h5>
+            <ListGroup>
+              {
+                quickLinks.map((item, index)=>(
+                  <ListGroupItem key={index} className='p-0 mt-3 quick__link'>
+                    <Link to={item.path}>{item.display}</Link>
+                  </ListGroupItem>
+                ))
+              }
+            </ListGroup>
+          </div>
+        </Col>
 
-export default Footer
+        <Col lg='3' md='4' sm='6'>
+          <div className="mb-">
+          <h5 className="footer__link-title mb-4">Head Office</h5>
+          <p className="office__info">123 Coding Building, Makati, Philippines</p>
+          <p className="office__info">Phone: +63 912 345 6789</p>
+          <p className="office__info">E-mail: carxpress@gmail.com</p>
+          <p className="office__info">Office Time (Monday-Friday): 8AM - 5PM</p>
+          </div>
+        </Col>
+
+        <Col lg='3' md='4'>
+          <div className="mb-4">
+            <h5 className="footer__link-title">Newsletter</h5>
+            <p className="section__description">Subscribe to Newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder='Email'/>
+              <span>
+                <i className="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </div>
+          </Col>
+
+      <Col lg="12">
+            <div className="footer__bottom">
+              <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
+                <i class="ri-copyright-line"></i>Copyright {year}. Developed by
+               Rick, Andrei and Jim. All rights reserved.
+              </p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
